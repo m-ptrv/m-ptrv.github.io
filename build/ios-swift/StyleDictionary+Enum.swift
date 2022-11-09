83ac0f3c0b425f300fc8c5fafb9da14d189d934e
m-ptrv <m-ptrv@users.noreply.github.com>
2022-11-09 09:51:17 +0000

    Commit from GitHub Actions (Update Styles)

--- build/ios-swift/StyleDictionary+Enum.swift
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 09:49:15 GMT
+// Generated on Wed, 09 Nov 2022 09:51:15 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let main = green
+    public static let main = yellow
     public static let tokenSetOrder0 = global
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 09:49:15 GMT
+// Generated on Wed, 09 Nov 2022 09:51:15 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let main = green
+    internal static let main = yellow
     internal static let tokenSetOrder0 = global
 }
