4bec6a538a5cbe199b6d1b2315c86509264676d9
m-ptrv <m-ptrv@users.noreply.github.com>
2022-11-09 18:06:23 +0000

    Commit from GitHub Actions (Update Styles)

--- build/ios-swift/StyleDictionary+Enum.swift
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -4,12 +4,101 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 11:58:39 GMT
+// Generated on Wed, 09 Nov 2022 18:06:21 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let main = green
+    public static let baseline = 4
+    public static let bodyLg = [object Object]
+    public static let bodyMd = [object Object]
+    public static let bodySm = [object Object]
+    public static let borderRadius = 4
+    public static let colorsBlack = #171717
+    public static let colorsBlue100 = #d8ddfd
+    public static let colorsBlue200 = #b1bafb
+    public static let colorsBlue300 = #8b98f9
+    public static let colorsBlue400 = #6475f7
+    public static let colorsBlue50 = #eceefe
+    public static let colorsBlue500 = #3d53f5
+    public static let colorsBlue600 = #3142c4
+    public static let colorsBlue700 = #253293
+    public static let colorsBlue800 = #182162
+    public static let colorsBlue900 = #0c1131
+    public static let colorsBlue950 = #060818
+    public static let colorsGreen100 = #e8f6ee
+    public static let colorsGreen200 = #d1eedd
+    public static let colorsGreen300 = #a3ddbc
+    public static let colorsGreen400 = #5dc389
+    public static let colorsGreen50 = #F4FBF7
+    public static let colorsGreen500 = #18a957
+    public static let colorsGreen600 = #11763d
+    public static let colorsGreen700 = #0a4423
+    public static let colorsGreen800 = #052211
+    public static let colorsGreen900 = #021109
+    public static let colorsGreen950 = #010905
+    public static let colorsGrey100 = #f5f5f5
+    public static let colorsGrey200 = #eeeeee
+    public static let colorsGrey300 = #e0e0e0
+    public static let colorsGrey400 = #bdbdbd
+    public static let colorsGrey50 = #fcfcfc
+    public static let colorsGrey500 = #9e9e9e
+    public static let colorsGrey600 = #757575
+    public static let colorsGrey700 = #616161
+    public static let colorsGrey800 = #424242
+    public static let colorsGrey900 = #323232
+    public static let colorsGrey950 = #1F1F1F
+    public static let colorsMint100 = #f4fafd
+    public static let colorsMint200 = #e9f6fa
+    public static let colorsMint300 = #ddf1f8
+    public static let colorsMint400 = #d2edf5
+    public static let colorsMint50 = #f9fdfe
+    public static let colorsMint500 = #c7e8f3
+    public static let colorsMint600 = #9fbac2
+    public static let colorsMint700 = #778b92
+    public static let colorsMint800 = #505d61
+    public static let colorsMint900 = #282e31
+    public static let colorsMint950 = #171A1C
+    public static let colorsPurple100 = #ddd9f7
+    public static let colorsPurple200 = #bbb2ef
+    public static let colorsPurple300 = #9a8ce8
+    public static let colorsPurple400 = #7865e0
+    public static let colorsPurple50 = #eeecfb
+    public static let colorsPurple500 = #563fd8
+    public static let colorsPurple600 = #4532ad
+    public static let colorsPurple700 = #342682
+    public static let colorsPurple800 = #221956
+    public static let colorsPurple900 = #110d2b
+    public static let colorsPurple950 = #090616
+    public static let colorsRed100 = #fce8ec
+    public static let colorsRed200 = #f9d0d9
+    public static let colorsRed300 = #f2a2b3
+    public static let colorsRed400 = #e95c7b
+    public static let colorsRed50 = #FDF1F4
+    public static let colorsRed500 = #df1642
+    public static let colorsRed600 = #9c0f2e
+    public static let colorsRed700 = #59091a
+    public static let colorsRed800 = #2d040d
+    public static let colorsRed900 = #160207
+    public static let colorsRed950 = #090103
+    public static let colorsWhite = #ffffff
+    public static let colorsYellow100 = #fff8eb
+    public static let colorsYellow200 = #fff1d7
+    public static let colorsYellow300 = #ffe4af
+    public static let colorsYellow400 = #ffcf74
+    public static let colorsYellow50 = #FFFBF5
+    public static let colorsYellow500 = #FFBB38
+    public static let colorsYellow600 = #b38327
+    public static let colorsYellow700 = #664b16
+    public static let colorsYellow800 = #33250b
+    public static let colorsYellow900 = #191306
+    public static let colorsYellow950 = #0C0903
+    public static let horizontalLg = 24
+    public static let horizontalMd = 20
+    public static let horizontalSm = 16
     public static let tokenSetOrder0 = global
+    public static let verticalLg = 16
+    public static let verticalMd = 12
+    public static let verticalSm = 8
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,101 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 11:58:39 GMT
+// Generated on Wed, 09 Nov 2022 18:06:21 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let main = green
+    internal static let baseline = 4
+    internal static let bodyLg = [object Object]
+    internal static let bodyMd = [object Object]
+    internal static let bodySm = [object Object]
+    internal static let borderRadius = 4
+    internal static let colorsBlack = #171717
+    internal static let colorsBlue100 = #d8ddfd
+    internal static let colorsBlue200 = #b1bafb
+    internal static let colorsBlue300 = #8b98f9
+    internal static let colorsBlue400 = #6475f7
+    internal static let colorsBlue50 = #eceefe
+    internal static let colorsBlue500 = #3d53f5
+    internal static let colorsBlue600 = #3142c4
+    internal static let colorsBlue700 = #253293
+    internal static let colorsBlue800 = #182162
+    internal static let colorsBlue900 = #0c1131
+    internal static let colorsBlue950 = #060818
+    internal static let colorsGreen100 = #e8f6ee
+    internal static let colorsGreen200 = #d1eedd
+    internal static let colorsGreen300 = #a3ddbc
+    internal static let colorsGreen400 = #5dc389
+    internal static let colorsGreen50 = #F4FBF7
+    internal static let colorsGreen500 = #18a957
+    internal static let colorsGreen600 = #11763d
+    internal static let colorsGreen700 = #0a4423
+    internal static let colorsGreen800 = #052211
+    internal static let colorsGreen900 = #021109
+    internal static let colorsGreen950 = #010905
+    internal static let colorsGrey100 = #f5f5f5
+    internal static let colorsGrey200 = #eeeeee
+    internal static let colorsGrey300 = #e0e0e0
+    internal static let colorsGrey400 = #bdbdbd
+    internal static let colorsGrey50 = #fcfcfc
+    internal static let colorsGrey500 = #9e9e9e
+    internal static let colorsGrey600 = #757575
+    internal static let colorsGrey700 = #616161
+    internal static let colorsGrey800 = #424242
+    internal static let colorsGrey900 = #323232
+    internal static let colorsGrey950 = #1F1F1F
+    internal static let colorsMint100 = #f4fafd
+    internal static let colorsMint200 = #e9f6fa
+    internal static let colorsMint300 = #ddf1f8
+    internal static let colorsMint400 = #d2edf5
+    internal static let colorsMint50 = #f9fdfe
+    internal static let colorsMint500 = #c7e8f3
+    internal static let colorsMint600 = #9fbac2
+    internal static let colorsMint700 = #778b92
+    internal static let colorsMint800 = #505d61
+    internal static let colorsMint900 = #282e31
+    internal static let colorsMint950 = #171A1C
+    internal static let colorsPurple100 = #ddd9f7
+    internal static let colorsPurple200 = #bbb2ef
+    internal static let colorsPurple300 = #9a8ce8
+    internal static let colorsPurple400 = #7865e0
+    internal static let colorsPurple50 = #eeecfb
+    internal static let colorsPurple500 = #563fd8
+    internal static let colorsPurple600 = #4532ad
+    internal static let colorsPurple700 = #342682
+    internal static let colorsPurple800 = #221956
+    internal static let colorsPurple900 = #110d2b
+    internal static let colorsPurple950 = #090616
+    internal static let colorsRed100 = #fce8ec
+    internal static let colorsRed200 = #f9d0d9
+    internal static let colorsRed300 = #f2a2b3
+    internal static let colorsRed400 = #e95c7b
+    internal static let colorsRed50 = #FDF1F4
+    internal static let colorsRed500 = #df1642
+    internal static let colorsRed600 = #9c0f2e
+    internal static let colorsRed700 = #59091a
+    internal static let colorsRed800 = #2d040d
+    internal static let colorsRed900 = #160207
+    internal static let colorsRed950 = #090103
+    internal static let colorsWhite = #ffffff
+    internal static let colorsYellow100 = #fff8eb
+    internal static let colorsYellow200 = #fff1d7
+    internal static let colorsYellow300 = #ffe4af
+    internal static let colorsYellow400 = #ffcf74
+    internal static let colorsYellow50 = #FFFBF5
+    internal static let colorsYellow500 = #FFBB38
+    internal static let colorsYellow600 = #b38327
+    internal static let colorsYellow700 = #664b16
+    internal static let colorsYellow800 = #33250b
+    internal static let colorsYellow900 = #191306
+    internal static let colorsYellow950 = #0C0903
+    internal static let horizontalLg = 24
+    internal static let horizontalMd = 20
+    internal static let horizontalSm = 16
     internal static let tokenSetOrder0 = global
+    internal static let verticalLg = 16
+    internal static let verticalMd = 12
+    internal static let verticalSm = 8
 }
