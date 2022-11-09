b835e1467aa77f182a71208bccaa0e3482abd561
Max P <mptrv.com@gmail.com>
2022-11-09 10:49:31 +0600

    Update

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 04:41:46 GMT
+// Generated on Wed, 09 Nov 2022 04:45:46 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let main = orange
+    public static let main = #ff0018
     public static let tokenSetOrder0 = global
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 04:41:46 GMT
+// Generated on Wed, 09 Nov 2022 04:45:46 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let main = orange
+    internal static let main = #ff0018
     internal static let tokenSetOrder0 = global
 }
