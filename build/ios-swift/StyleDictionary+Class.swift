82fbb885900d1588011db3c78b187fd546de0c21
m-ptrv <m-ptrv@users.noreply.github.com>
2022-11-09 09:43:25 +0000

    Commit from GitHub Actions (Update Styles)

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 09:41:19 GMT
+// Generated on Wed, 09 Nov 2022 09:43:23 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let main = #ffb200
+    public static let main = green
     public static let tokenSetOrder0 = global
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 09 Nov 2022 09:41:19 GMT
+// Generated on Wed, 09 Nov 2022 09:43:23 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let main = #ffb200
+    internal static let main = green
     internal static let tokenSetOrder0 = global
 }
