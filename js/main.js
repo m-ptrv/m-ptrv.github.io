99d0ea5b03611d6ca05d32d067ed9090de7e459d
Maksim Petrov <maksim.petrov@devexpress.com>
2022-11-11 20:27:33 +0600

    Add styles and logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-setTimeout(() => window.location.reload(), 60000);
+var newLink;
+
+function switchTheme() {
+    if (newLink) {
+        newLink.remove();
+        newLink = false;
+        return;
+    }
+
+    var link = document.createElement( "link" );
+    var number = Math.random() * 1000000;
+    link.href = "css/dark" + ".css" + "?" + Math.floor(number);
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    
+    document.getElementsByTagName( "head" )[0].appendChild( link );   
+    
+    newLink = $(link);
+    console.log(newLink)
+}
+
