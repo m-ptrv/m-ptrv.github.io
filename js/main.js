895d44c1d5d0e7c645181f19b86c7beb74f20f41
Maksim Petrov <maksim.petrov@devexpress.com>
2022-11-21 21:55:44 +0600

    Update styles

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var newLink;
 var newLinkNum = 0;
 
 function switchTheme() {
-    if (newLink && newLinkNum === 2) {
+    if (newLink && newLinkNum === 3) {
         newLink.remove();
         newLink = false;
         newLinkNum = 0;
@@ -12,7 +12,7 @@ function switchTheme() {
     if (newLinkNum === 0) {
         var link = document.createElement( "link" );
         var number = Math.random() * 1000000;
-        link.href = "css/dark" + ".css" + "?" + Math.floor(number);
+        link.href = "css/rainy_ashville" + ".css" + "?" + Math.floor(number);
         link.type = "text/css";
         link.rel = "stylesheet";
         
@@ -27,7 +27,7 @@ function switchTheme() {
         newLink.remove();
         var link = document.createElement( "link" );
         var number = Math.random() * 1000000;
-        link.href = "css/green" + ".css" + "?" + Math.floor(number);
+        link.href = "css/dark" + ".css" + "?" + Math.floor(number);
         link.type = "text/css";
         link.rel = "stylesheet";
         
@@ -35,6 +35,21 @@ function switchTheme() {
         
         newLink = $(link);
         newLinkNum = 2;
+        return;
+    }
+
+    if (newLinkNum === 2) {
+        newLink.remove();
+        var link = document.createElement( "link" );
+        var number = Math.random() * 1000000;
+        link.href = "css/green" + ".css" + "?" + Math.floor(number);
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        
+        document.getElementsByTagName( "head" )[0].appendChild( link );   
+        
+        newLink = $(link);
+        newLinkNum = 3;
         return;
     }
 }
