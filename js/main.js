05ebfe311e2cd2074a998d033218c655eb71f292
Maksim Petrov <maksim.petrov@devexpress.com>
2022-11-21 19:36:39 +0600

    Update styles render

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
 var newLink;
+var newLinkNum = 0;
 
 function switchTheme() {
-    if (newLink) {
+    console.log(newLinkNum)
+    if (newLink && newLinkNum === 2) {
         newLink.remove();
         newLink = false;
+        newLinkNum = 0;
         return;
     }
 
-    var link = document.createElement( "link" );
-    var number = Math.random() * 1000000;
-    link.href = "css/dark" + ".css" + "?" + Math.floor(number);
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    
-    document.getElementsByTagName( "head" )[0].appendChild( link );   
-    
-    newLink = $(link);
-    console.log(newLink)
+    if (newLinkNum === 0) {
+        var link = document.createElement( "link" );
+        var number = Math.random() * 1000000;
+        link.href = "css/dark" + ".css" + "?" + Math.floor(number);
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        
+        document.getElementsByTagName( "head" )[0].appendChild( link );   
+        
+        newLink = $(link);
+        newLinkNum = 1;
+        return;
+    }
+
+    if (newLinkNum === 1) {
+        newLink.remove();
+        var link = document.createElement( "link" );
+        var number = Math.random() * 1000000;
+        link.href = "css/green" + ".css" + "?" + Math.floor(number);
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        
+        document.getElementsByTagName( "head" )[0].appendChild( link );   
+        
+        newLink = $(link);
+        newLinkNum = 2;
+        return;
+    }
 }
 
